07aef16302d4bee7e55797f3172e4d1a3e2f82c4
Hernan Jorge Urich <hernan.urich@gmail.com>
2025-03-31 15:08:53 -0300

    se realizo ejercicio2_3 de Python

---
+++ 03 Estructuras Condicionales/ejercicio2_3.py
@@ -0,0 +1,10 @@
+""" 2) Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, 
+deberá mostrar por pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar 
+el mensaje “Desaprobado”. """ 
+
+nota = int(input("Ingrese la nota obtenida en la materia: "))
+
+if nota >= 6:
+    print("APROBADO")
+else:
+    print("DESAPROBADO")
