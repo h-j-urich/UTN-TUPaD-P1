ffc4c049e4bd1fc291711be9eaea8669b124ad6e
Hernan Jorge Urich <hernan.urich@gmail.com>
2025-03-31 15:18:26 -0300

    se realizo ejercicio3_3

---
+++ 03 Estructuras Condicionales/Trabajos Practicos 03/ejercicio3_.py
@@ -0,0 +1,12 @@
+""" 3) Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un 
+número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso 
+contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del 
+operador de módulo (%) en Python para evaluar si un número es par o impar. """
+
+num1 = int(input("ingrese un numero par: "))
+
+if num1 % 2 == 0:
+    print ("Ha ingresado un numero par")
+
+else:
+    print("Por favor, ingrese un numero par")
