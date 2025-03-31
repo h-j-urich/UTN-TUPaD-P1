b163049f83c60b8c541dbb77c1ce1b37ab7696fe
Hernan Jorge Urich <hernan.urich@gmail.com>
2025-03-31 16:35:12 -0300

    Se realizo ejercicio10_3

---
+++ 03 Estructuras Condicionales/Trabajos Practicos 03/ejercicio10_3.py
@@ -0,0 +1,20 @@
+""" 10) Utilizando la información aportada en la siguiente tabla sobre las estaciones del año 
+Periodo del año: 
+Desde el 21 de diciembre hasta el 20 de 
+marzo (incluidos) 
+Desde el 21 de marzo hasta el 20 de junio 
+(incluidos) 
+Desde el 21 de junio hasta el 20 de 
+septiembre (incluidos) 
+Desde el 21 de septiembre hasta el 20 de 
+diciembre (incluidos)
+
+Estación en el hemisferio norte:
+Invierno, primavera, verano, otoño
+
+Estación en el hemisferio norte:
+verano, otoño, invierno, primavera
+
+Escribir un programa que pregunte al usuario en cuál hemisferio se encuentra (N/S), qué mes 
+del año es y qué día es. El programa deberá utilizar esa información para imprimir por pantalla 
+si el usuario se encuentra en otoño, invierno, primavera o verano."""
