ecec1f24debf310b3f14f989e8492930ee324289
Hernan Jorge Urich <hernan.urich@gmail.com>
2025-04-01 09:17:35 -0300

    se realizaron cambios en la estructura condicional

--- 03 Estructuras Condicionales/Trabajos Practicos 03/ejercicio9_3.py
+++ 03 Estructuras Condicionales/Trabajos Practicos 03/ejercicio9_3.py
@@ -9,3 +9,19 @@ generalmente no causa daños).
 débiles). 
 ● Mayor o igual que 6  y menor que 7: "Muy Fuerte" (puede causar daños significativos). 
 ● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala).  """
+
+magnitud = float(input("Ingrese la magnitud del terremoto: "))
+
+if magnitud < 3:
+    print("Magnitud muy leve(imperceptible)")
+elif magnitud >= 3 and magnitud < 4:
+    print("Magnitud leve (ligeramente perceptible)")
+elif magnitud >= 4 and magnitud < 5:
+    print("Magnitud moderado (sentido por personas, pero generalmente no causa daños)")
+elif magnitud >= 5 and magnitud < 6:
+    print("Magnitud fuerte (puede causar daños en estructuras debiles)")
+elif magnitud >= 6 and magnitud < 7:
+    print("Magnitud muy fuerte (puede causar daños significativos)")
+elif magnitud >= 7:
+    print("Magnitud extremo (puede causar daños graves a gran escala)")
+
