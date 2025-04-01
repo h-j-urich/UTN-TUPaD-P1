9fbe194d433d181dd73ccbeb8d870cb6d9d75f6e
Hernan Jorge Urich <hernan.urich@gmail.com>
2025-04-01 09:25:59 -0300

    se modifico estructura condicional

--- 03 Estructuras Condicionales/Trabajos Practicos 03/ejercicio10_3.py
+++ 03 Estructuras Condicionales/Trabajos Practicos 03/ejercicio10_3.py
@@ -18,3 +18,50 @@ verano, otoño, invierno, primavera
 Escribir un programa que pregunte al usuario en cuál hemisferio se encuentra (N/S), qué mes 
 del año es y qué día es. El programa deberá utilizar esa información para imprimir por pantalla 
 si el usuario se encuentra en otoño, invierno, primavera o verano."""
+
+# Pedir al usuario el hemisferio, el mes y el día
+hemisferio = input("¿En qué hemisferio te encuentras? N para norte y S para sur: ").strip().upper()
+mes = int(input("Introduce el numero de mes (1-12): "))
+dia = int(input("Introduce el día (1-31): "))
+
+# Validar la estación según el hemisferio y la fecha
+if hemisferio == "N":
+    if (mes == 12 and dia >= 21) or (mes in [1, 2]) or (mes == 3 and dia <= 20):
+        estacion = "Invierno"
+    elif (mes == 3 and dia >= 21) or (mes in [4, 5]) or (mes == 6 and dia <= 20):
+        estacion = "Primavera"
+    elif (mes == 6 and dia >= 21) or (mes in [7, 8]) or (mes == 9 and dia <= 20):
+        estacion = "Verano"
+    else:
+        estacion = "Otoño"
+
+elif hemisferio == "S":
+    if (mes == 12 and dia >= 21) or (mes in [1, 2]) or (mes == 3 and dia <= 20):
+        estacion = "Verano"
+    elif (mes == 3 and dia >= 21) or (mes in [4, 5]) or (mes == 6 and dia <= 20):
+        estacion = "Otoño"
+    elif (mes == 6 and dia >= 21) or (mes in [7, 8]) or (mes == 9 and dia <= 20):
+        estacion = "Invierno"
+    else:
+        estacion = "Primavera"
+else:
+    estacion = "Hemisferio no válido"
+
+print(f"La estación correspondiente es: {estacion}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
